b1369517075d26e29c8228c4d65602768b2054f4
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-08-20 04:25:20 +0100

    Fix Rubocop errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
-module UsersHelper
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test 'should get hello' do
+    get pages_hello_url
+    assert_response :success
+  end
 end
